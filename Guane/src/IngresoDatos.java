aa38a753d49eb3894bf66ec679119b1b0f2fbaf7
Wilson <wilsonrueda2@gmail.com>
2020-09-28 22:16:09 -0500

    Se crea una clase padre para hacer el codigo mas facil de leer

---
+++ Guane/src/IngresoDatos.java
@@ -0,0 +1,12 @@
+
+public class IngresoDatos extends Datos{
+	
+//Metodo para ingresar datos mediante pantalla.
+	
+	public void ingresarDatos() {
+		System.out.println("Ingresar dato: ");
+		temperatura=ingresar.next();
+		
+	}
+
+}
---
+++ Guane/src/Resultado.java
@@ -0,0 +1,9 @@
+
+public class Resultado extends AnalisisDatos {
+
+	public static void main(String[] args) {
+		AnalisisDatos analiza=new AnalisisDatos();
+		analiza.ingresarDatos();
+		analiza.mostrar();
+	}
+}
